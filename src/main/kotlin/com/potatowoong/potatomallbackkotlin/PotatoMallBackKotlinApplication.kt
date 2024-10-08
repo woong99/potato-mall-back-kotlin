3ef2ff69ae3dddb061ba310e1bf6db5ec3548ce6
woong99 <dndry1072@naver.com>
2024-10-08 10:50:44 +0900

    init project

---
+++ src/main/kotlin/com/potatowoong/potatomallbackkotlin/PotatoMallBackKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.potatowoong.potatomallbackkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PotatoMallBackKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<PotatoMallBackKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/potatowoong/potatomallbackkotlin/PotatoMallBackKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.potatowoong.potatomallbackkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PotatoMallBackKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
